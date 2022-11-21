879c815fffac1216fea1df148ea8baf242f2dd8c
jmt <2968773430@qq.com>
2022-11-21 15:59:57 +0800

    这是写文件上传的开始

--- src/main/java/com/zf/bk2/controller/BookController.java
+++ src/main/java/com/zf/bk2/controller/BookController.java
@@ -13,7 +13,10 @@ import lombok.extern.slf4j.Slf4j;
 import ma.glasnost.orika.MapperFactory;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URLEncoder;
 import java.util.List;
 
 import static java.lang.System.out;
@@ -40,6 +46,12 @@ public class BookController {
     @Autowired
     private IDocService docService;
 
+    private Doc doc = new Doc();
+
+    private Book book = new Book();
+
+    private PageBean pageBean = new PageBean();
+
 
     private  MapperFactory mapperFactory;
     private com.zf.bk2.util.UrlUtils UrlUtils;
@@ -50,7 +62,7 @@ public class BookController {
 
     @RequestMapping("/toList")
     @ResponseBody//获取前端json数据的注释代码
-    public  String toList(BookVo bookVo, PageBean pageBean, HttpServletRequest request){
+    public  String toList(BookVo bookVo,HttpServletRequest request){
         out.println("查询从前端查到的book所有的数据："+bookVo);
         out.println("查询从前端查到的pageBean所有的数据："+pageBean);
         List<BookVo> bookList = bookService.list(bookVo, pageBean);
@@ -72,7 +84,7 @@ public class BookController {
 
     @RequestMapping("toMerger")
     @ResponseBody
-    public  String tomerger( Book book,HttpServletRequest request){
+    public  String tomerger(HttpServletRequest request){
 
         JsonData jsonData = new JsonData();
         jsonData.setCode(0);
@@ -133,7 +145,7 @@ public class BookController {
 
     @RequestMapping("todelete")
     @ResponseBody
-    public  String delete( Book book,HttpServletRequest request){
+    public  String delete(HttpServletRequest request){
         JsonData jsonData = new JsonData();
         jsonData.setCode(0);
         String bookId = request.getParameter("bookId");
@@ -148,27 +160,29 @@ public class BookController {
 
     @RequestMapping("toUploadBookImage")
     @ResponseBody
-    public  String uploadBookImage( HttpServletRequest request,PageBean pageBean)throws Exception{
+    public  String uploadBookImage( HttpServletRequest request){
        //获取前端的值
         String bookName = request.getParameter("bookName");
+        book.setBookName(bookName);
         out.println("输出从前端获取的姓名："+bookName);
-        //获取前端页码页大小
-        String page = request.getParameter("page");
-        String rows = request.getParameter("rows");
-        //转换数据为int类型
-        int pageInt = Integer.parseInt(page);
-        int rowsInt = Integer.parseInt(rows);
-        //输出页码页大小
-        out.println("输出前端获取的页码："+page);
-        out.println("输出从前端获取的页大小:"+rows);
-        //赋值
-        pageBean.setRows(pageInt);
-        pageBean.setRows(rowsInt);
-        //进行分页查询
-        return  null;
+        JsonData jsonData = new JsonData();
+        BookVo bookVo = new BookVo();
+        List<BookVo> docList = bookService.list(bookVo, pageBean);
+        out.println("返回到前端值：" + docList);
+        jsonData.setCode(0);
+        jsonData.setMessage("书籍查询成功");
+        jsonData.setResult(docList);
+        jsonData.setPage(pageBean.getPage());
+        jsonData.setRows(pageBean.getRows());
+        jsonData.setTotal(pageBean.getTotal());
+        String JsonData = JsonUtils.getJson(jsonData);
+        out.println("查询返回前端的数据：" + JsonData);
+        return JsonData;
     }
 
-    public  String uploadBookImage(HttpServletRequest request,MultipartFile file,Doc doc,Book book)throws  Exception{
+    @RequestMapping("uploadBookImage")
+    @ResponseBody
+    public  String uploadBookImage(HttpServletRequest request,MultipartFile file)throws  Exception{
         out.println("查看img："+file);
         out.println("进入文件上传");
         //获取书本Id
@@ -184,22 +198,82 @@ public class BookController {
         System.out.println("查看值下一步");
         //2.保存文件上传记录
         doc.setId(id);
+        out.println("获取："+id);
         doc.setFileName(file.getOriginalFilename());
+        out.println("获取："+file.getOriginalFilename());
         doc.setMime(file.getContentType());
+        out.println("获取:"+file.getContentType());
         docService.insert(doc);
+        out.println("获取增加后的值："+doc);
         //更新书本封面
         book.setBookId(bookIdLong);
+        out.println("获取更新后的bookId:"+bookIdLong);
         book.setBookImage(id);
+        out.println("获取从doc里面传过来的id赋值给bookImage："+id);
         Book loadBook = bookService.loadBook(book);
-        out.println("查询一遍："+loadBook.getBookName());
+        out.println("获取查询的book:"+book);
         book.setBookName(loadBook.getBookName());
-
-//        bookService.update1(this.book);
-//        out.println("修改后id" + this.book);
+        out.println("查询一遍："+loadBook.getBookName());
+        bookService.update1(book);
+        out.println("修改后id" + book);
         JsonData jsonData = new JsonData();
         jsonData.setCode(0);
         jsonData.setMessage("封面上传成功");
         String JsonData = JsonUtils.getJson(jsonData);
         return JsonData;
+    }
+
+    @RequestMapping("asOpen")//查看文件上传成功的图片
+    @ResponseBody
+    public ResponseEntity asOpen(HttpServletRequest request, long id)throws  Exception{
+        System.out.println("查看id:" + id);
+        doc.setId(id);
+        Doc d = docService.loadImage(id);
+        out.println("对比d结果：" + d);
+        String fileName = d.getFileName();
+        //io读写文件
+        byte[] body = null;
+        String path = saveDir + d.getId();
+        System.out.println(path);
+        InputStream in = new FileInputStream(path);
+        body = new byte[in.available()];
+        in.read(body);
+        HttpHeaders headers = new HttpHeaders();
+        out.println("查看mime"+d.getMime());
+        headers.add("Content-Type", d.getMime());
+        out.println("查看fileName"+fileName);
+        headers.add("Content-Disposition", ";filename=" + fileName);
+        HttpStatus statusCode = HttpStatus.OK;
+        ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(body, headers, statusCode);
+        out.println("最终:" + response);
+        return response;
+    }
+
+    //文件下载
+    @RequestMapping("/asSave")
+    @ResponseBody
+    public ResponseEntity asSave(Long id) throws Exception {
+//        String id = request.getParameter("bookImage");
+//        long longBookId = Long.parseLong(id);
+        out.println("是否能获取到id："+id);
+        doc.setId(id);
+        Doc d = docService.loadImage(id);
+        String fileName = d.getFileName();
+        fileName = URLEncoder.encode(fileName, "UTF-8");
+
+        //io读写文件
+        byte[] body = null;
+        String path = saveDir + d.getId();
+        System.out.println(path);
+        InputStream in = new FileInputStream(path);
+        body = new byte[in.available()];
+        in.read(body);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", d.getMime());
+        headers.add("Content-Disposition", "attachment;filename=" + fileName);
+        HttpStatus statusCode = HttpStatus.OK;
+        ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(body, headers, statusCode);
+        return response;
     }
 }
